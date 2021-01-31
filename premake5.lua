8b20a9c16639e963088e20663b7bbbceefc24a4c
Daniil Ellovich <49017080+ellovich@users.noreply.github.com>
2021-02-01 01:51:06 +0300

    bin dirs added to gitignore

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,34 @@
 project "ImGui"
-  kind "StaticLib"
-  language "C++"
-  
-  targetdir ("bin/" .. outputDir .. "/%{prj.name}")
-  objdir ("bin/" .. outputDir .. "/%{prj.name}")
-  
-  files
-  {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
-  }
-  
-  filter "system:windows"
-    systemversion "latest"
+    kind "StaticLib"
+    language "C++"
     cppdialect "C++17"
-    staticruntime "On"
+    staticruntime "on"
+  
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
     
-  filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+    filter "system:windows"
+        systemversion "latest"
+        
+    	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
